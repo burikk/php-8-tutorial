92b9811531d7100a2d4239c13828865974782e4e
oleksandr burkotenko <o.burkotenko@lemonmind.com>
2022-08-16 23:55:19 +0200

    constants examples

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,14 @@
 <?php
 
-phpinfo();
+//constants
+if (true) {
+    define('HELLO_WORLD', 'Hello World!');
+    //you can't declare const variables like this in control structures
+    //const HELLO_WORLD = 'Hello World!';
+}
+
+const MY_CONST_VAR = 'Here you can declare const by the keyword const';
+//this way you can check if const is defined, and echo that variables
+echo defined('HELLO_WORLD'), defined('MY_CONST_VAR');
+echo HELLO_WORLD, MY_CONST_VAR;
+echo PHP_VERSION, __FILE__; //predefined constant
