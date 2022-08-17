45ffe5d3fe5183f7459e2b519028ceff98841aa8
oleksandr burkotenko <o.burkotenko@lemonmind.com>
2022-08-17 11:22:52 +0200

    booleans, integers, floats, arrays, strings, null data types

--- src/public/index.php
+++ src/public/index.php
@@ -11,4 +11,75 @@ const MY_CONST_VAR = 'Here you can declare const by the keyword const';
 //this way you can check if const is defined, and echo that variables
 echo defined('HELLO_WORLD'), defined('MY_CONST_VAR');
 echo HELLO_WORLD, MY_CONST_VAR;
-echo PHP_VERSION, __FILE__; //predefined constant
+echo PHP_VERSION, __FILE__, '<pre>'; //predefined constant
+
+//Booleans
+$isTrue = true;
+// 0, -0, 0.0, -0.0, '', '0', [], null = false
+
+//Integers
+$x = PHP_INT_MAX + 1; //getting float
+$x = (int) true; //1
+$x = (int) false; //0
+$x = (int) 5.9999; //5
+$x = (int) '5.999'; //5
+$x = (int) '23qweqwe'; //23
+
+//Floats
+$y = 13.5e3; //13500
+$y = 13.5e-3; //0.0135
+$y = PHP_FLOAT_MAX + 1;//INF
+is_finite($x); //false, checks if not INF
+//<0.5 - floor, >0.5 - ceil to get right integers, 0.5 gives you a normal res
+$y = floor((0.1 + 0.7) * 10); //7, not 8
+echo $y;
+$y = ceil((0.1 + 0.7) * 10);//8
+echo $y;
+$y = ceil((0.1 + 0.2) * 10);//4
+echo $y . '<pre>';
+$x = 0.23;
+$y = 1 - 0.77;
+//$x == $y, $x !== $y;
+
+//NULL
+$x = null;//echo nothing, '', 0, false, [], var_dump() return NULL
+is_null($x);//true
+
+//Strings
+//Heredoc
+$text = <<<TEXT
+Line 1 $y
+Line 2
+TEXT;
+echo $text;
+//Nowdoc
+$text = <<<'TEXT'
+Line 1 $y
+Line 2
+TEXT;
+echo $text . '<pre>';
+
+//Arrays
+$fruits = ['banana', 'apple', 'peach'];
+var_dump(isset($fruits[1])); //true
+var_dump(isset($fruits[3])); //false
+var_dump(count($fruits));//3
+$fruits[] = 'strawberry';// push new item to array fruits
+array_push($fruits, 'kiwi', 'orange');
+var_dump(count($fruits));//6
+$osVersions = [
+    'ios' => '15.1',
+    'android' => '11.2',
+]; // define array keys
+echo $osVersions['ios'];
+$osVersions[] = 'undefined os'; //add new array key value with not specified key
+$osVersions['windows'] = '11'; //assign value to the specific key
+$osVersions['windows'] = [
+    'name' => '11',
+    'ver' => 'lite',
+    'nullValue' => null,
+];
+var_dump(array_key_exists($osVersions['windows']['nullValue'], $osVersions));
+print_r($osVersions);
+$array = [true => 'a', 1 => 'b', '1' => 'c', 1.8 => 'd', null => 'e'];//prints [1] => d [] => e
+
