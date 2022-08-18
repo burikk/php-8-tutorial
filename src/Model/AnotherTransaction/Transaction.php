7d7efd8974d3ca692cac91f9dc8753f3a4bf6b2f
oleksandr burkotenko <o.burkotenko@lemonmind.com>
2022-08-18 22:53:04 +0200

    add custom autoloader

--- src/Model/AnotherTransaction/Transaction.php
+++ src/Model/AnotherTransaction/Transaction.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace AnotherTransaction;
+namespace App\Model\AnotherTransaction;
 
 class Transaction
 {
--- src/Model/Transaction/Transaction.php
+++ src/Model/Transaction/Transaction.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Transaction;
+namespace App\Model\Transaction;
 
 class Transaction
 {
