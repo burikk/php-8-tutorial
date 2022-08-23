a91c75a83728833e3dd08950e5fac169949b760e
oleksandr burkotenko <o.burkotenko@lemonmind.com>
2022-08-23 16:24:17 +0200

    add env support for php and mysql pdo

--- public/index.php
+++ public/index.php
@@ -12,6 +12,10 @@ use App\View;
 //});
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
 const VIEW_PATH = __DIR__ . '/../views';
 
 try {
